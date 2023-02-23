4d181c524734e77ffcba6f204a0581f58ab69ebf
Abdulmalikmat <119923912+Abdulmalikmat@users.noreply.github.com>
2023-02-23 11:07:11 +0300

    Update error_handlers.py

--- service/common/error_handlers.py
+++ service/common/error_handlers.py
@@ -28,7 +28,11 @@ def bad_request(error):
         status.HTTP_400_BAD_REQUEST,
     )
 
-
+    def test_get_account_not_found(self):
+        """It should not Read an Account that is not found"""
+        resp = self.client.get(f"{BASE_URL}/0")
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+    
 @app.errorhandler(status.HTTP_404_NOT_FOUND)
 def not_found(error):
     """Handles resources not found with 404_NOT_FOUND"""
